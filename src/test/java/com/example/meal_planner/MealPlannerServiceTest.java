abfc74fd968a41ed52dd24207375b74933ffb526
BjørnEilerås <26852670+bjorneileras@users.noreply.github.com>
2022-12-15 15:21:06 +0100

    Database class editing, testing more shit... DONT USE

--- src/test/java/com/example/meal_planner/MealPlannerServiceTest.java
+++ src/test/java/com/example/meal_planner/MealPlannerServiceTest.java
@@ -1,11 +1,27 @@
 package com.example.meal_planner;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class MealPlannerServiceTest {
+
+    @Autowired
+    RecipeDatabase recipeDatabase;
+
+
+    @org.junit.Test
+    public void testTest() {
+
+    }
+
+
+
 
     /*
     @Test
